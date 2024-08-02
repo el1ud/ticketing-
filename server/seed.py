8f65c888d6f1c8fe79ef8f73ef4defbaf923fd5b
eliud wainaina <wainainaeliud7@gmail.com>
2024-08-02 09:19:58 -0700

    finish seedpy

--- server/seed.py
+++ server/seed.py
@@ -28,3 +28,38 @@ def seed_concerts(n=5):
         db.session.add(concert)
     db.session.commit()
 
+def seed_tickets(n=20):
+    users = User.query.all()
+    concerts = Concert.query.all()
+    
+    for _ in range(n):
+        ticket = Ticket(
+            seat_number=fake.word(),
+            price=fake.random_number(digits=2),
+            user_id=fake.random_element(elements=[user.id for user in users]),
+            concert_id=fake.random_element(elements=[concert.id for concert in concerts])
+        )
+        db.session.add(ticket)
+    db.session.commit()
+
+def seed_concert_users(n=15):
+    users = User.query.all()
+    concerts = Concert.query.all()
+    
+    for _ in range(n):
+        concert_user = ConcertUser(
+            user_id=fake.random_element(elements=[user.id for user in users]),
+            concert_id=fake.random_element(elements=[concert.id for concert in concerts]),
+            user_rating=fake.random_number(digits=1)
+        )
+        db.session.add(concert_user)
+    db.session.commit()
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        seed_users()
+        seed_concerts()
+        seed_tickets()
+        seed_concert_users()
+        print("Database seeded!")
